c81b3e77627b0955fb469d996c2127b7c66fb9ed
it21268694 <99591042+it21268694@users.noreply.github.com>
2024-09-29 19:20:38 +0530

    section7.2

--- resources/views/components/section7.blade.php
+++ resources/views/components/section7.blade.php
@@ -1,10 +1,12 @@
-<div class="flex flex-col bg-[#DFE7ED] w-full h-[823px]">
-    <div class="flex-1 ml-[7.3%] z-10 mt-36">
+<div class="flex flex-col h-screen bg-[#DFE7ED] w-full">
+    <div class="flex-1 w-full h-[50%]">
         <p class="text-[#ECBD00] text-[75%] font-bold p-0 m-0">Find Us Fast</p>
         <p class="text-[150%] font-bold p-0 m-0">Let's Discuss Your Property Goals.</p>
     </div>
 
-    <div class="flex justify-center">
+    <div class=" flex-1 w-full h-[50%]">
         <img class=" w-full" src="{{ asset('images/property/anju/Map.png') }}" alt="map">
     </div>
+
+
 </div>
