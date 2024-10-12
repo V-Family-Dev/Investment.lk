e0501ceb2b5487d6a69299df7a50d05f2936e334
sanjanadilhara <sanjanadilhara2000@gmail.com>
2024-10-12 10:02:37 +0530

    top header

--- resources/views/adminPanel/admin/dashboard.blade.php
+++ resources/views/adminPanel/admin/dashboard.blade.php
@@ -58,22 +58,59 @@
                 </div>
             </div>
         </div>
-        <div class="overflow-y-scroll overflow-x-hidden vh-100 flex-fill position-relative main-container">
-            <div class="position-fixed top-50 left-0  shadow-sm w-100 header-bar px-3 bg-success">
-                ddf
-                <!-- <div class="d-flex justify-content-between align-items-center w-50">
-                    <div>
-                        admin - dashboard
+        <div class="overflow-x-hidden vh-100 flex-fill position-relative main-container d-flex flex-column">
+            <div class="header-bar w-100 bg-white shadow-sm d-flex align-items-center px-3 justify-content-between">
+                <div class="text-secondary fs-6">
+                    admin <i class="fa-solid fa-arrow-right"></i> dashboard
+                </div>
+                <div>
+                    <div class="" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img width="40" height="40" src="{{ asset('images/person/zak.png') }}" alt="" class="rounded-circle object-fit-cover" >
+                        <i class="fa-solid fa-caret-down ps-2 text-secondary"></i>
+                        <div class="dropdown">
+                            <ul class="dropdown-menu mt-3">
+                                <li class="">
+                                    <a class="dropdown-item d-flex py-3" href="#">
+                                        <img width="60" height="60" src="{{ asset('images/person/zak.png') }}" alt="" class=" rounded-circle object-fit-cover ">
+                                        <div class="ps-3">
+                                            <div>name name</div>
+                                            <div class="text-secondary">
+                                                example@gmail.com
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item d-flex justify-content-between align-items-center py-2" href="#">
+                                        <div>Logout</div> 
+                                        <i class="fa-solid fa-right-from-bracket"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <div>
-                        <img width="50" height="50" src="{{ asset('images/person/zak.png') }}" alt="">
-                    </div>
-                </div> -->
+                </div>
             </div>
-
-            <div>
+            <div class="overflow-y-scroll overflow-x-hidden flex-fill">
                 <div class="bg-white p-4 rounded-4 shadow m-5">
-                    fgg
+                    <div class="fs-6 text-secondary">title</div>
+                    <div class="fs-5">title title</div>
+
+                    <div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">Name</label>
+                            <input
+                                type="text"
+                                class="form-control"
+                                name=""
+                                id=""
+                                aria-describedby="helpId"
+                                placeholder=""
+                            />
+                            <small id="helpId" class="form-text text-muted">Help text</small>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
 
